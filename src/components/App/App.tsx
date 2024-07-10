446b19d2659a8ab3ab0dab20b0775b91980c4e4a
Oleg122 <joeburen122@gmail.com>
2024-07-10 11:25:47 +0300

    first

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import { CurrencyTrans } from "../CurrencyTrans";
+import classes from './App.module.scss'
+
+export const App = () => {
+    return ( 
+        <div>
+            <CurrencyTrans/>
+        </div>
+    );
+}
+
+export default App;
